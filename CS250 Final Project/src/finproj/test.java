d22bb8d0d5a99b7d2825bff9df2225c1f2f5f123
warrenm <warrenm@gmail.com>
2025-03-26 10:54:16 -0500

    gfdgfdgfdgfdgfd thiw is a better message
    Change-Id: I7c1c561f796ac4a81c97c5f26db7cf5d2406ca43

---
+++ CS250 Final Project/src/finproj/test.java
@@ -0,0 +1,15 @@
+package finproj;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi, this is a test.");
+		System.out.println("Hi, this is another test.");
+		
+		}
+	test
+	
+hgfhgf
+}
